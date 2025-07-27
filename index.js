3b94d8fe8578b067768a6534e137e4c75f9a32a8
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-27 10:16:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+// Serve static files like style.css
+app.use(express.static(__dirname));
+
+// Serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Socket.IO handlers
+io.on('connection', (socket) => {
+    socket.on('send name', (user) => {
+        io.emit('send name', user);
+    });
+
+    socket.on('send message', (chat) => {
+        io.emit('send message', chat);
+    });
+});
+
+const port = 5000;
+server.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+});
